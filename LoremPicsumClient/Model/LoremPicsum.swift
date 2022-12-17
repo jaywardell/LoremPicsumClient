4ac2eeb1c3fa3fbdd8dac2bb1f9b6295610bdd64
Joseph Wardell <jaywardell@mac.com>
2022-12-17 07:07:39 -0500

    added a LoremPicsum model enum and a test module for it

---
+++ LoremPicsumClient/Model/LoremPicsum.swift
@@ -0,0 +1,10 @@
+//
+//  LoremPicsum.swift
+//  LoremPicsumClient
+//
+//  Created by Joseph Wardell on 12/17/22.
+//
+
+import Foundation
+
+enum LoremPicsum {}
---
+++ LoremPicsumClientTests/LoremPicsumTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoremPicsumTests.swift
+//  LoremPicsumClientTests
+//
+//  Created by Joseph Wardell on 12/17/22.
+//
+
+import XCTest
+
+final class LoremPicsumTests: XCTestCase {
+
+
+}
