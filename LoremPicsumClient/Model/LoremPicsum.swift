0a2d622e25632966c029a0b3a314b3cdd5dbab16
Joseph Wardell <jaywardell@mac.com>
2022-12-17 07:39:04 -0500

    added an extension on URLCOmpoenents to make a more elegant way to construct the URLs. refactored the methods on LoremPicsum to use that etension

--- LoremPicsumClient/Model/LoremPicsum.swift
+++ LoremPicsumClient/Model/LoremPicsum.swift
@@ -10,16 +10,53 @@ import Foundation
 enum LoremPicsum {
     // see https://picsum.photos for documenation on the API for LoremPicsum
 
+    private static let host = "picsum.photos"
+    private static var base: URLComponents {
+        var out = URLComponents()
+        out.host = host
+        out.scheme = "https"
+        return out
+    }
+    
     static func randomPicture(width: Int, height: Int) -> URL {
-        URL(string: "https://picsum.photos/\(width)/\(height)")!
+        base
+            .addingPathComponent(width)
+            .addingPathComponent(height)
+            .url!
     }
     
     static func randomPicture(square width: Int) -> URL {
-        URL(string: "https://picsum.photos/\(width)")!
+        base
+            .addingPathComponent(width)
+            .url!
     }
 
     static func picture(id: Int, width: Int, height: Int) -> URL {
-        URL(string: "https://picsum.photos/id/\(id)/\(width)/\(height)")!
+        base
+            .addingPathComponent("id")
+            .addingPathComponent(id)
+            .addingPathComponent(width)
+            .addingPathComponent(height)
+            .url!
+    }
+}
+
+// MARK: -
+
+extension URLComponents {
+    func with(path: String) -> URLComponents {
+        var out = self
+        out.path = path
+        return out
     }
     
-}
+    func addingPathComponent(_ string: String) -> URLComponents {
+        var out = self
+        out.path += "/" + string
+        return out
+    }
+    
+    func addingPathComponent(_ int: Int) -> URLComponents {
+        addingPathComponent(String(int))
+    }
+ }
