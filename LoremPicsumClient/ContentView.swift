77dbd3d95b9aaaec86e8b099d751b2251ff634de
Joseph Wardell <jaywardell@mac.com>
2022-12-17 06:33:12 -0500

    Initial Commit

---
+++ LoremPicsumClient/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LoremPicsumClient
+//
+//  Created by Joseph Wardell on 12/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoremPicsumClient/LoremPicsumClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremPicsumClientApp.swift
+//  LoremPicsumClient
+//
+//  Created by Joseph Wardell on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LoremPicsumClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
